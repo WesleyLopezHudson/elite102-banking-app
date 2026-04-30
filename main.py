c30d43c196e8b33efe50ff5f2d0affaa4e33f18b
Wesley Lopez-Hudson <wesleylopezhudson@gmail.com>
2026-04-30 02:40:47 -0500

    Added the option to delete accounts and fixed crashes with try/except Value errors and by specifiining the variable type

--- main.py
+++ main.py
@@ -23,9 +23,10 @@ def banking_system():
                         "\n1. Open an account" 
                         "\n2. View balance" \
                         "\n3. Withdraw balance" \
-                        "\n4. Deposit balance" \
+                         "\n4. Deposit balance" \
                         "\n5. View all account details" \
-                        "\n6. Exit" \
+                        "\n6. Delete an account" \
+                        "\n7. Exit" \
                         "\n"
                         "\n"
                             )
@@ -51,19 +52,22 @@ def banking_system():
 
     elif start_screen == "2":
                 #Query Data
-        account_id = input("\nWhat was your account id?\n") #This chooses which accounts balance to view
-        cursor.execute("SELECT * FROM accounts WHERE id = " + account_id) # The * makes it show everything in the account's row
-        rows = cursor.fetchone()
-        print("The information displayed below is your id, name, and balance in that order.\n\n")
-        for row in rows:
-            print(row)
+        try:
+            account_id = float(input("\nWhat was your account id?\n")) #This chooses which accounts balance to view
+            cursor.execute("SELECT * FROM accounts WHERE id = " + str(account_id)) # The * makes it show everything in the account's row
+            rows = cursor.fetchone()
+            print("The information displayed below is your id, name, and balance in that order.\n\n")
+            for row in rows:
+                print(row)
+        except ValueError:
+            print("Please use a number for your id next time")
 
 
 
     elif start_screen == "3":
         try:
-            account_id = input("\nWhat was your account id?\n") #This chooses which account to withdraw from
-            cursor.execute("SELECT balance FROM accounts WHERE id = " + account_id)
+            account_id = float(input("\nWhat is your account id?\n")) #This chooses which account to withdraw from
+            cursor.execute("SELECT balance FROM accounts WHERE id = " + str(account_id))
             current_balance = cursor.fetchone() #Fetches and stores the account as a variable for later use
             if current_balance == None: # Checks if accounts exists
                 print("There isn't an account with that ID")
@@ -80,7 +84,8 @@ def banking_system():
 
         except ValueError: # Checks if an actual number was entered
             print("Sorry, your withdraw ammount isn't a valid number")
-
+            return
+        
         current_balance_float -= withdraw_ammount # This removes the withdrawn ammount
         print("You have withdrawn $" + str(withdraw_ammount) + " from your account. You now have " + str(current_balance_float) + " in your account.")
         cursor.execute("UPDATE accounts SET balance = " + str(current_balance_float) + " WHERE id = " + str(account_id))
@@ -89,8 +94,8 @@ def banking_system():
 
     elif start_screen == "4":
         try:
-            account_id = input("\nWhat was your account id?\n") #This chooses which account to deposit to
-            cursor.execute("SELECT balance FROM accounts WHERE id = " + account_id)
+            account_id = float(input("\nWhat is your account id?\n")) #This chooses which account to deposit to
+            cursor.execute("SELECT balance FROM accounts WHERE id = " + str(account_id))
             current_balance = cursor.fetchone() #Fetches and stores the account as a variable for later use
             if current_balance == None: # Checks if accounts exists
                 print("There isn't an account with that ID")
@@ -107,7 +112,8 @@ def banking_system():
 
         except ValueError: # Checks if an actual number was entered
             print("Sorry, your deposit ammount isn't a valid number")
-
+            return
+        
         current_balance_float += withdraw_ammount # This removes the withdrawn ammount
         print("You have deposited $" + str(withdraw_ammount) + " from your account. You now have $" + str(current_balance_float) + " in your account.")
         cursor.execute("UPDATE accounts SET balance = " + str(current_balance_float) + " WHERE id = " + str(account_id))
@@ -121,7 +127,35 @@ def banking_system():
         for row in rows:
             print(row)
 
-    elif start_screen == "6":    
+
+
+    elif start_screen == "6":
+        try:
+            account_id = float(input("\nWhat is your account id?\n"))
+            
+        except ValueError: # Checks if an actual number was entered
+            print("Sorry, your didn't enter in a valid ID number")
+            return
+
+        cursor.execute("SELECT * FROM accounts WHERE id = " + str(account_id))
+        rows = cursor.fetchone()
+        for row in rows:
+            print(row)
+        try:
+            last_chance = float(input("\nIs this your information? 1 for yes, 2 for no:\n"))
+            if last_chance == 1:
+                cursor.execute("DELETE FROM accounts WHERE id = " + str(account_id))
+                print("Account deleted successfully.")
+            elif last_chance == 2:
+                print("If you want to delete your account, make sure its your id you enter.")
+            else:
+                print("You didn't enter in one of the given options, terminating deletion process.")
+        except ValueError:
+            print("You didn't enter in one of the given options, terminating deletion process.")
+            return
+
+        
+    elif start_screen == "7":    
         print("Connection Terminated"
         "\n")
         pass #exit() wouldn't allow anything outside of the function to happen, like saving data
